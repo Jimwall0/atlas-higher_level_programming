f5a801007ad5baedfa424bc8ff65fb91f57f207e
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 18:58:51 -0500

    print if statement

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for call in range(len(matrix)):
         for index in range(len(matrix[call])):
-            string = "{:d} "
-            print(string.format(matrix[call][index]), end="")
+            string = "{:d}"
+            print(string.format(matrix[call][index]), end=" " if index != 2 else "\n")
         print()
