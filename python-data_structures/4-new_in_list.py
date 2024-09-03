f79d745fa136bf3195b2cab8c0876b6ac54f6ead
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 17:13:57 -0500

    changed to actual name

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,8 +2,8 @@
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return list(my_list)
-    l = list(my_list)
+    copy = list(my_list)
     for num in range(len(l)):
         if idx == num:
-            l[num] = element
-    return l
+            copy[num] = element
+    return copy
