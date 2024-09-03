7bf5aa6e2f9d66608b9494e94ced379f298083a3
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 09:44:24 -0500

    start from 1

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for x in range(10):
-    for i in range(10):
+    for i in range(1, 10):
         if i < x:
             continue
         print("{}{}".format(x, i), end=", " if i != 10 else "")
