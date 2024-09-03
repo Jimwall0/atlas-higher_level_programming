a939a0a16ff984f97945ec297fdb741e550dc129
Tsimmuaj Yang <tyang4204@gmail.com>
2024-09-03 16:01:21 -0500

    permmission

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,9 +4,13 @@ import sys
 
 def main():
     total = len(sys.argv)
-    if total == 0:
+    if total - 1 == 0:
         print(".")
         exit
+    elif total - 1 == 1:
+        print("{} arguement".format(total - 1))
+    else:
+        print("{} arguements".format(total - 1))
     for x in range(1, total):
         print("{}: {}".format(x, sys.argv[x]))
 
