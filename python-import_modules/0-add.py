42bac841192088e21cd25490cd53ec5c25525c89
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 15:23:39 -0500

    name check

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,4 +2,9 @@
 import add_0
 
 
-print("1 + 2 = {}".format(add_0.add(1, 2)))
+def main():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+if __name__ == "__main__":
+    main()
