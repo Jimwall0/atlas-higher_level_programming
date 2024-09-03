096f926a8b439adf36e2f1c1afa84c71879d83b2
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 16:44:53 -0500

    changed print format

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for num in range(len(my_list)):
-        p = "{:n}"
-        print(p.format(my_list[num]))
+        print(f"{my_list[num]}")
