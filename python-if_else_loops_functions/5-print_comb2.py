efb4a8d3173abc9cacabe58435aa5998aebbb696
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 09:28:20 -0500

    obscure if statement method

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for x in range(100):
-    print("{:02d}".format(x), end="")
-    if x != 99:
-        print(", ", end="")
+    print("{:02d}".format(x), end=", " if x != 99 else "")
+print()
