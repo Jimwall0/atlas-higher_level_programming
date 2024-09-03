182219f9f28d6ef447083203af1d6d2262462c7e
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 14:49:44 -0500

    add function

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return a ^ b
+    if b == 0:
+        return 1
+    elif b < 0:
+        number = float(1 / a)
+        for loop in range (b - 1):
+            number *= number
+        return number
+    for loop in range (b - 1):
+        number *= number
+    return number
+    
