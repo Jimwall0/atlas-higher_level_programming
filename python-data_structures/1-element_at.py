391e3c2bf3444ed110a7c0cf184afa2c6633de5f
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 16:52:58 -0500

    changed array to in

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    for num in len(my_list):
+    for num in range(len(my_list)):
         if idx == num:
             return my_list[num]
     return None
