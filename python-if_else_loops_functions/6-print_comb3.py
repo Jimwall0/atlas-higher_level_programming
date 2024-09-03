361df69631775dd3e9bc4ac09a4018b30ea04fba
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 09:51:49 -0500

    rm duplicates

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,7 @@ for x in range(10):
     for i in range(1, 10):
         if i < x:
             continue
+        elif i == x:
+            continue
         print("{}{}".format(x, i), end=", " if x != 9 else "")
 print()
