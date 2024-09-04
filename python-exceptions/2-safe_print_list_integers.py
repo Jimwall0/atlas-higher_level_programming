be1cc0704bb6570dfad2a16df7946370ab7e2c11
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 21:49:29 -0500

    typeerror

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[loop]), end="")
                 count += 1
 
+    except TypeError:
+        pass
     except IndexError:
         pass
     print()
