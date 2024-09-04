61d5f5c5967b997b0179050e8264bb6a39381919
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 21:15:13 -0500

    empty print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
-        if x == 0:
-            return 0
         for num in range(x):
-            if not my_list[num]:
-                raise IndexError
-            else:
-                print("{}".format(my_list[num]), end="")
-        print()
-        return num + 1
+            print("{}".format(my_list[num]), end="")
+            count += 1
     
     except IndexError:
-        return num
+        pass
+    print()
+    return count
