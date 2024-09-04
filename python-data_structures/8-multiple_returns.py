85affc6d5a856337b9e467adfa790ea03d7385af
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 20:02:39 -0500

    removed junk code

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,7 +3,3 @@ def multiple_returns(sentence):
     if not sentence or sentence == None:
         return tuple((0, None))
     return tuple((len(sentence), sentence[0]))
-
-sentence = "Hello world"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
