e89918fc4b6e01afc3f83f43db235452a425392a
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 21:40:06 -0500

    Safe print int

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for loop in range(x):
+            if isinstance(my_list[loop]):
+                print("{:d}".format(my_list[loop]), end="")
+                count += 1
+
+    except Exception:
+        pass
+    print()
+    return count
