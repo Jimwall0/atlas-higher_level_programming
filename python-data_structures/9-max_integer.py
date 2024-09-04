afc25ed7f5d14add4fbd5faf4ec90115c6c5359f
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 20:07:43 -0500

    changed for loop

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if not my_list or my_list is None:
         return None
     max = 0
-    for loop in my_list:
+    for loop in range(len(my_list)):
         if my_list[loop] > max:
             max = my_list[loop]
     return max
