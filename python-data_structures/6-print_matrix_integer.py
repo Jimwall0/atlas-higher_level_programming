1bd52714de33fd527578912941ea79980b5c6542
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 19:05:21 -0500

    got rid of index

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for call in range(len(matrix)):
-        for index in range(len(matrix[call])):
+        for i in range(len(matrix[call])):
             string = "{:d}"
             a = len(matrix[call]) - 1
-            print(string.format(matrix[call][index]), end=" " if index != a else "")
+            print(string.format(matrix[call][i]), end=" " if i != a else "")
         print()
