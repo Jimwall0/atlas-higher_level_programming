2e3e46efb6842b43c040d7d805bd08f13e3e748e
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 19:03:38 -0500

    changed to check len

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     for call in range(len(matrix)):
         for index in range(len(matrix[call])):
             string = "{:d}"
-            print(string.format(matrix[call][index]), end=" " if index != 2 else "")
+            print(string.format(matrix[call][index]), end=" " if index != len(matrix[call]) - 1 else "")
         print()
