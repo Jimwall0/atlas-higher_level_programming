ccbc3230d14ec1738b23e39a20d6b3160e3d00ed
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 19:57:21 -0500

    None

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 def multiple_returns(sentence):
     if sentence == None:
         return None
-    if sentence == "": return (tuple((0), None))
+    if not sentence: return tuple((0, None))
     return tuple((len(sentence), sentence[0]))
