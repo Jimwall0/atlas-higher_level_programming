f228b7ba2f4f827f8d2d174c53db4931f40a7927
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 21:06:32 -0500

    assigned num

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        num = 0
         for num in range(x):
             if not my_list[num]:
                 raise IndexError
