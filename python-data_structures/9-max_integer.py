de8dc8ae0cee22ad80f9b82e1510257c488f0c97
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 20:08:35 -0500

    start of with a variable in list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if not my_list or my_list is None:
         return None
-    max = 0
+    max = my_list[0]
     for loop in range(len(my_list)):
         if my_list[loop] > max:
             max = my_list[loop]
