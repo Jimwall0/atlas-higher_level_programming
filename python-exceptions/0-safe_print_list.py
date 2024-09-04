f209fe3924c164e1ffaaffb7bac7027e4b85c4a1
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 21:16:00 -0500

    formating

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for num in range(x):
             print("{}".format(my_list[num]), end="")
             count += 1
-    
+
     except IndexError:
         pass
     print()
