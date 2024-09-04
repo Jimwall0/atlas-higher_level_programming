ee8a283a354d75d70004ffa18ccce6f8c6171e48
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 22:37:46 -0500

    save

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,11 +5,17 @@ def list_division(my_list_1, my_list_2, list_length):
         for loop in range(list_length):
             newlist.insert(loop, my_list_1[loop] / my_list_2[loop])
 
-    except TypeError as e:
+    except TypeError:
+        newlist.insert(loop, 0)
         print("wrong type")
-    except ZeroDivisionError as e:
+
+    except ZeroDivisionError:
+        newlist.insert(loop, 0)
         print("out of range")
-    except IndexError as e:
+
+    except IndexError:
+        newlist.insert(loop, 0)
         print("division by 0")
+
     finally:
         return newlist
