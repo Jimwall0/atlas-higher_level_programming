5dc7966978e1d6bf04e814a3d3e398df6bd9626d
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 20:02:00 -0500

    added more checks

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == None:
-        return None
-    if not sentence: return tuple((0, None))
+    if not sentence or sentence == None:
+        return tuple((0, None))
     return tuple((len(sentence), sentence[0]))
+
+sentence = "Hello world"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
