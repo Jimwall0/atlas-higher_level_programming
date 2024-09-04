2a94948e52672030c7a10cbe4b563bfc4cd1a310
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 20:03:23 -0500

    used is

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence or sentence == None:
+    if not sentence or sentence is None:
         return tuple((0, None))
     return tuple((len(sentence), sentence[0]))
