802d9842a4dbfacced5d0eab1153d6cb1967a9f6
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 21:05:38 -0500

    working safe print

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for num in range(x):
+            if not my_list[num]:
+                raise IndexError
+            else:
+                print("{}".format(my_list[num]), end="")
+        print()
+        return num + 1
+    
+    except IndexError:
+        return num
