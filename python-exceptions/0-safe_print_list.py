809d1666083e80a69606a1d256e386fb26a59b4f
Tsimmuaj <tyang4204@gmail.com>
2024-09-03 21:07:21 -0500

    0 check

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        num = 0
+        if x == 0:
+            return 0
         for num in range(x):
             if not my_list[num]:
                 raise IndexError
