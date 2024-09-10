4e8772a60d5dee6651b78237e3d3ed3c30e34e31
Tsimmuaj <tyang4204@gmail.com>
2024-09-10 16:06:40 -0500

    added rectagle.py file

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+This is code for a rectangle class
+"""
+
+
+class Rectangle:
+    pass
--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+This is a test for test driven development
+"""
+
+
 def add_integer(a, b=89):
+    """
+    This methods adds two integers together
+    """
     if isinstance(a, float) or isinstance(b, float):
         a = int(a)
         b = int(b)
