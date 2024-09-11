8093bc5d54794dc19ed015aa0a4d2411501412f3
Tsimmuaj <tyang4204@gmail.com>
2024-09-10 19:38:42 -0500

    Runs

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -15,19 +15,19 @@ class Rectangle:
     @property
     def width(self):
         return self._width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        self.width = value
-    
+        self._width = value
+
     @property
     def height(self):
         return self._height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
