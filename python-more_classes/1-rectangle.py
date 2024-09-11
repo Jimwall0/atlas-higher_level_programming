67d39627109ea4b8b6481cb2753b86db935adf3a
Tsimmuaj <tyang4204@gmail.com>
2024-09-10 19:44:41 -0500

    added case in init

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,7 +9,15 @@ class Rectangle:
     This is the class for rectangle
     """
     def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
         self._width = width
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
         self._height = height
 
     @property
