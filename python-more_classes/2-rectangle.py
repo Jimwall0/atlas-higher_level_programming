a1d8c0379fd5b293d5a9949fb76c113ec7a0e2bf
Tsimmuaj <tyang4204@gmail.com>
2024-09-10 20:12:40 -0500

    perimeter 0 case

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -48,4 +48,6 @@ class Rectangle:
         return self._Rectangle__height * self._Rectangle__width
 
     def perimeter(self):
+        if self._Rectangle__width == 0 or self._Rectangle__height == 0:
+            return 0
         return 2(self._Rectangle__height + self._Rectangle__width)
