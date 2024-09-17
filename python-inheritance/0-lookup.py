152b26736871f10f692d23893aeb6c9005951424
Tsimmuaj <tyang4204@gmail.com>
2024-09-17 17:07:51 -0500

    doc

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+This is a file to contain a lookup function
+"""
 def lookup(obj):
+    """
+    This is the lookup function
+    that returns the methods and arguements
+    """
     return vars(obj)
