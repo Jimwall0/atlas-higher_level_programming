d24774ad81a91fcb5ad68f671f6456e9095ae93e
Tsimmuaj <tyang4204@gmail.com>
2024-09-17 18:30:40 -0500

    save

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Write a function that reads a text file (UTF8) and prints it to stdout
+"""
+
+def read_file(filename=""):
+    """
+    This is the method to read a file
+    """
+
+    with open(filename) as file:
+        print(file.read())
