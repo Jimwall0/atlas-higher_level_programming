5a45f437f7d5b8bcc6d7c52bff4935958f3c98ab
Tsimmuaj <tyang4204@gmail.com>
2024-09-17 18:17:23 -0500

    test

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,15 +2,33 @@
 """
 Write a class MyList that inherits from list
 """
+
+
 class MyList(list):
     """
     This is a class containing function and list attributes
+
+    Example:
+    
+    >>> my_list = MyList()
+    >>> my_list.extend([3, 1, 4, 1, 5, 9, 2, 6])
+    >>> my_list.print_sorted()
+    [1, 1, 2, 3, 4, 5, 6, 9]
     """
     def __init__(self):
         super().__init__()
+
     def print_sorted(self):
+
         """
         This prints a sorted list
+
+        Example:
+
+        >>> my_list = MyList()
+        >>> my_list.extend([3, 1, 4, 1, 5, 9, 2, 6])
+        >>> my_list.print_sorted()
+        [1, 1, 2, 3, 4, 5, 6, 9]
         """
         temp = self.copy()
         temp.sort()
