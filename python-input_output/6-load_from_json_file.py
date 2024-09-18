54dfd58df28ee645417bd4b13194d027f0d615a8
Tsimmuaj <tyang4204@gmail.com>
2024-09-17 19:38:10 -0500

    loads from a json file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Write a function that creates an
+Object from a “JSON file”
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Grabs a json and makes an object
+    """
+
+    with open(filename, "w") as file:
+        json.loads(file.read())
