e8bfe14e75e8616a070ba903cf0a9e910b32a957
Tsimmuaj <tyang4204@gmail.com>
2024-09-23 14:49:19 -0500

    text save

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,7 +2,6 @@
 """
 Write a class MyList that inherits from list
 """
-import doctest
 
 
 class MyList(list):
@@ -20,16 +19,7 @@ class MyList(list):
 
         """
         This prints a sorted list
-
-        Example:
-
-        >>> my_list = MyList()
-        >>> my_list.extend([3, 1, 4, 1, 5, 9, 2, 6])
-        >>> my_list.print_sorted()
-        [1, 1, 2, 3, 4, 5, 6, 9]
         """
         temp = self.copy()
         temp.sort()
         print(temp)
-
-doctest.testmod()
