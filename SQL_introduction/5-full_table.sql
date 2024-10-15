bf8a0ead954e44fe1350ab7a46537c314a6dc996
Tsimmuaj <tyang4204@gmail.com>
2024-10-15 13:26:45 -0500

    added create table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- Write a script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA 
-FROM information_schema.COLUMNS 
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' 
-AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
