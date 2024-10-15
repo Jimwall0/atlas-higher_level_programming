6efd528c7f173e540390f17afc414e7bb83e23ac
Tsimmuaj <tyang4204@gmail.com>
2024-10-15 18:55:02 -0500

    change group by

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY COUNT(tv_show_genres.genre_id) ASC, 
+GROUP BY tv_genres.id, tv_genres.name
 ORDER BY number_of_shows DESC;
