65c096f714de8bb124dd18a7aac5217331418d34
Tsimmuaj <tyang4204@gmail.com>
2024-10-15 18:57:02 -0500

    exclude zero

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,4 +3,5 @@ SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_show
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.id, tv_genres.name
+HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
