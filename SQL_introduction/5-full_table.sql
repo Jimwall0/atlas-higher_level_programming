c6c6d2f643a8934c4298f081acd5d51618da85e8
Tsimmuaj <tyang4204@gmail.com>
2024-10-15 13:14:52 -0500

    used a different method

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,5 @@
 -- Write a script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SHOW COLUMNS FROM first_table;
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA 
+FROM information_schema.COLUMNS 
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' 
+AND TABLE_NAME = 'first_table';
