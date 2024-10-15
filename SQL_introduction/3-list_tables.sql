3c87ea9a325b44f038b1b362c114bfb41d85826e
Tsimmuaj <tyang4204@gmail.com>
2024-10-15 12:24:19 -0500

    show tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,5 +1,2 @@
 -- Write a script that lists all the tables of a database in your MySQL server.
-SELECT table_name
-FROM information_schema.tables
-WHERE table_type='BASE TABLE'
-    AND table_schema = 'hbtn_0c_0'
+SHOW TABLES;
