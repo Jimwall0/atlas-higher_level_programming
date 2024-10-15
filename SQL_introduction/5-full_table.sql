5a4fa71cadbcd32f3c343e0ebeaa990a9c484025
Tsimmuaj <tyang4204@gmail.com>
2024-10-15 13:10:27 -0500

    show columns

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,12 +1,2 @@
 -- Write a script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT 
-    COLUMN_NAME, 
-    DATA_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_DEFAULT, 
-    CHARACTER_MAXIMUM_LENGTH 
-FROM 
-    information_schema.columns 
-WHERE 
-    table_schema = 'hbtn_0c_0' 
-    AND table_name = 'first_table';
+SHOW COLUMNS FROM first_table;
