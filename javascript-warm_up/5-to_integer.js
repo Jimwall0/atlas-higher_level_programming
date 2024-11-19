0f054ffffb2208cee1c579707c7920f4acd13d3a
Tsimmuaj <tyang4204@gmail.com>
2024-11-19 17:42:05 -0600

    corrected output message

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const tmp = process.argv[2];
 if (Number.isInteger(Number(tmp))) {
-  console.log(tmp);
+  console.log('My number: ' + tmp);
 } else {
   console.log('Not a number');
 }
