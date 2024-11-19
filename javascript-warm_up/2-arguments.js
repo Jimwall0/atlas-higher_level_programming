3ecde1ae7ba1efb5d9f1e59286c2457274ccae94
Tsimmuaj <tyang4204@gmail.com>
2024-11-19 17:10:54 -0600

    fixed spelling error

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@ const len = process.argv.length;
 if (len < 3) {
   console.log('No Arguement');
 } else if (len === 3) {
-  console.log('Argumenet found');
+  console.log('Arguement found');
 } else {
   console.log('Arguements found');
 }
