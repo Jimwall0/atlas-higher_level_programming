1937edfaeb0c78c5168f95d843322ef121dd0041
Tsimmuaj <tyang4204@gmail.com>
2024-11-19 19:26:37 -0600

    semistandard

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 function factorial (n) {
   if (n === 0 || n === 1) {
-      return 1;
-    }
+    return 1;
+  }
   return n * factorial(n - 1);
 }
 console.log(factorial(process.argv[2]));
