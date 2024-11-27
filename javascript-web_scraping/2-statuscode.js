3a88f373c8a0b02db81bdd526c84b92d64233e47
Tsimmuaj <tyang4204@gmail.com>
2024-11-26 20:18:30 -0600

    semistandard

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,8 +3,8 @@ const request = require('request');
 const url = process.argv[2];
 request(url, (err, request) => {
   if (err) {
-    console.log("Error:", err);
+    console.log('Error:', err);
   } else {
-    console.log("code:", request.statusCode);
+    console.log('code:', request.statusCode);
   }
 });
