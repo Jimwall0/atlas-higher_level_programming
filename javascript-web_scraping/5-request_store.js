6f64e02375af5260ff0f5d8d45fc46da31995573
Tsimmuaj <tyang4204@gmail.com>
2024-11-26 21:17:28 -0600

    correct output

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const path = process.argv[3];
+request(url, (err, response, body) => {
+  if (err) {
+    console.log('Error:', err);
+  } else if (response.statusCode === 200) {
+    fs.writeFile(path, body, 'utf-8', (err) => {
+      if (err) {
+        console.log('fail');
+      } else {
+        console.log('Works');
+      }
+    });
+  } else {
+    console.log('Not found');
+  }
+});
